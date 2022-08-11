e7ccd7db5d29d3ec868ac6aa0d17c5c987f10dcc
YukiSugaya <sugaya324@gmail.com>
2022-08-11 23:08:52 +0900

    fix: fix getKey on useSWR for query

--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -5,7 +5,7 @@ export const useQuestions = (query?: ParsedUrlQuery) => {
   const q = new URLSearchParams(encode(query)).toString();
   const fetcher = (url: RequestInfo) => fetch(url).then((res) => res.json());
   const { data, error, mutate, size, setSize } = useSWRInfinite(
-    (index) => `/api/questions?page=${index + 1}&=${q}`,
+    (index) => `/api/questions?page=${index + 1}&${q}`,
     fetcher,
     {
       revalidateOnFocus: false,
