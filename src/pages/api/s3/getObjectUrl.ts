caea13a6b70544bad00067d3ee2e11d29c2a3c7a
YukiSugaya <sugaya324@gmail.com>
2022-09-14 14:40:48 +0900

    fix: modify env variables name

--- src/pages/api/s3/getObjectUrl.ts
+++ src/pages/api/s3/getObjectUrl.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { key } = req.query;
-    const s3ObjectUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+    const s3ObjectUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.MY_AWS_REGION}.amazonaws.com/${key}`;
     res.status(200).json({ s3ObjectUrl });
   } catch (error) {
     res.status(400).json({ message: error });
--- src/s3/s3Client.ts
+++ src/s3/s3Client.ts
@@ -1,9 +1,9 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
 export const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.MY_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY || '',
   },
 });
